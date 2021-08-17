c95915e63097a1667eac18438a9ca15c37fe3b4e
kimengu-david <davidmutune200@gmail.com>
2021-08-17 23:38:59 +0000

    Add task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -17,7 +17,7 @@ class Config(object):
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-app.config.from_object('1-app.Config')
+app.config.from_object(Config)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
